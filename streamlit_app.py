7415cd468445bd8cd099d588814f6c09f6a4b3e4
Neku <nipapa@my.cspc.edu.ph>
2025-03-27 03:51:08 +0000

    update title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,3 @@
 import streamlit as st
 
-st.title("Applied Cryptography Encryptonator" )
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Applied Cryptography Encryptonator 🕵" )
